b45ff7872995d0fb0c715ef5fd84a71fd7c09e9e
b1llywitant0 <billywitanto@yahoo.co.id>
2024-10-20 01:10:34 +0700

    refactor: changing logic of app churn rate

--- semantic_dbt/models/marketing/marketing_churn_rate.sql
+++ semantic_dbt/models/marketing/marketing_churn_rate.sql
@@ -1,7 +1,9 @@
+-- Defining churn rate of sellers if they didn't generate sales in the next three months after won
+
 SELECT 
 	mtcd.year_won,
 	mtcd.month_won,
-	TO_CHAR(ROUND((1 - ROUND(mcdr.mql_retention_count,2 )/ROUND(mtcd.total_won,2))*100.00,2), 'fm00D00%') AS churn_rate 
+	TO_CHAR(ROUND((1 - ROUND(mcdr.mql_retention_count,2 )/ROUND(mtcd.total_won,2))*100.00,2), 'fm990.00%') AS churn_rate 
 FROM {{ref("marketing_total_closed_deals")}} mtcd 
 LEFT JOIN {{ref("marketing_closed_deals_retention")}} mcdr 
 ON mtcd.year_won = mcdr.year_won AND mtcd.month_won = mcdr.month_won
